2efb8aa834248aca4f3a9503df0825200e53b1c3
shreyashHonrao <shreyashhonrao8@gmail.com>
2025-02-28 22:44:54 -0800

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'team';
+  title = 'I love Angular from TS File';
+  skills : Array<string> = ["Html","JS","TS","Angular"]
+  // names = 'Name of Colour';
+  name : Array<string> = ["dark","orange","Blue","white"]
+  // change = 'prime numbers';
+  changes : Array<string> = ["2","3","5","7"]
+  // odd = 'Odd Numbers';
+  odds : Array<string> = ["11","21","31","41"]
+  // subject = 'subject Name';
+  subjects : Array<string> = ["English","Math","Science","Hindi"]
 }
+
