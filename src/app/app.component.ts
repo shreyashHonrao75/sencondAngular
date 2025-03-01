3141cc122633a8a7a7908b34e33df0c66038d96e
shreyashHonrao <shreyashhonrao8@gmail.com>
2025-03-01 15:27:39 -0800

    https://github.com/shreyashHonrao75/sencondAngular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Subject } from 'rxjs';
+import { Icars, Iperson } from './shared/models/persons';
 
 @Component({
   selector: 'app-root',
@@ -8,13 +10,86 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'I love Angular from TS File';
   skills : Array<string> = ["Html","JS","TS","Angular"]
-  // names = 'Name of Colour';
-  name : Array<string> = ["dark","orange","Blue","white"]
-  // change = 'prime numbers';
-  changes : Array<string> = ["2","3","5","7"]
-  // odd = 'Odd Numbers';
-  odds : Array<string> = ["11","21","31","41"]
-  // subject = 'subject Name';
-  subjects : Array<string> = ["English","Math","Science","Hindi"]
+  
+
+  persons : Array<Iperson> = [
+    {
+      fname : "Jhon",
+      lname : "Doe",
+      email : "jd@gmail.com",
+      Contact : 8764434353
+    },
+    {
+      fname : "krishna",
+      lname : "Biradar",
+      email : "krishna@gmail.com",
+      Contact : 632641221
+    },
+    
+    {
+      fname : "Ritesh",
+      lname : "patil",
+      email : "Ritesh@gmail.com",
+      Contact : 786343631
+    },
+    
+    {
+      fname : "omkar",
+      lname : "Mane",
+      email : "omakr@gmail.com",
+      Contact : 643464646
+    },
+    
+    {
+      fname : "rajesh",
+      lname : "patil",
+      email : "rajesh@gmail.com",
+      Contact : 84572421343
+    },
+    
+    {
+      fname : "Jhon",
+      lname : "Doe",
+      email : "jd@gmail.com",
+      Contact : 5343343334
+    },
+  ]
+
+  
+  cars : Array<Icars> = [
+    {
+      car : "Creta",
+      Company : "hyundai",
+      kmDriven : 45236,
+      price : "9 Lakhs"
+    },
+    {
+      car : "Venue",
+      Company : "hyundai",
+      kmDriven : 63535,
+      price : "6 lakhs"
+    },
+    {
+      car : "Aura",
+      Company : "hyundai",
+      kmDriven : 65655,
+      price : "4 Lakhs"
+    },
+    {
+      car : "Ertiga",
+      Company : "Maruti Suzuki",
+      kmDriven : 20000,
+      price : "8 Lakhs"
+    },
+    {
+      car : "Ciaz",
+      Company : "Maruti Suzuki",
+      kmDriven : 53354,
+      price : "6 Lakhs"
+    },
+
+  ]
+
+  
 }
 
---
+++ src/app/shared/models/persons.ts
@@ -0,0 +1,13 @@
+export interface Iperson {
+    fname : string,
+    lname : string,
+    email : string,
+    Contact : number
+}
+
+export interface Icars {
+    car : string,
+    Company : string,
+    kmDriven : number,
+    price : string
+}
